341e2d67b0d576b6d437dc2a15c50a2cf710a582
gnamithan <gnaganathanahalli@ucsd.edu>
2022-02-25 13:19:48 -0800

    changed to reviewed code

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -15,22 +15,14 @@ public class MarkdownParseTest {
         // assertEquals(2, 1 + 1);
 
         //String contentsTest = Files.readString(Path.of("test-file.md"));
-        //String contentsTest1 = Files.readString(Path.of("test1.md"));
+        String contentsTest1 = Files.readString(Path.of("test1.md"));
         //String contentsTest2 = Files.readString(Path.of("test2.md"));
-        String contentsTest3 = Files.readString(Path.of("test3.md"));
+        //String contentsTest3 = Files.readString(Path.of("test3.md"));
 
-       // assertEquals(List.of("https://something.com", "some-page.html"), 
-            //MarkdownParse.getLinks(contentsTest));
-        //assertEquals(List.of("https://something.com"), 
-            //MarkdownParse.getLinks(contentsTest1));
-        //assertEquals(List.of("someLink.html"), 
-            //MarkdownParse.getLinks(contentsTest2));
-       // assertEquals(List.of("someLink.html"), 
-           // MarkdownParse.getLinks(contentsTest3));
-        //assertEquals(List.of("[a link](url.com)", "another link`", "cod[e" , "code]"), 
-            //MarkdownParse.getLinks(contentsTest1));
-        //assertEquals(List.of("[a nested link](b.com)", "a nested parenthesized url", "some escaped [ brackets ]"), 
-            //MarkdownParse.getLinks(contentsTest2));
+        assertEquals(List.of("[a link](url.com)", "another link`", "cod[e" , "code]"), 
+            MarkdownParse.getLinks(contentsTest1));
+        /* assertEquals(List.of("[a nested link](b.com)", "a nested parenthesized url", "some escaped [ brackets ]"), 
+            MarkdownParse.getLinks(contentsTest2));
         assertEquals(List.of("[this title text is really long and takes up more than one line", 
                     "and has some line breaks]( https://www.twitter.com )",
                      "this title text is really long and takes up more than one line",
@@ -39,6 +31,6 @@ public class MarkdownParseTest {
                      "[this link doesn't have a closing parenthesis for a while](https://cse.ucsd.edu/",
                       ")",
                      "And then there's more text"), 
-            MarkdownParse.getLinks(contentsTest3));
+            MarkdownParse.getLinks(contentsTest3)); */
     }
 }
