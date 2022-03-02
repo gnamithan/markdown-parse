b0cc90ba800ada2078135440463438280d26d651
gnamithan <gnaganathanahalli@ucsd.edu>
2022-03-02 12:13:04 -0800

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -34,6 +34,7 @@ public class MarkdownParse {
         if(dirOrFile.isDirectory()) {
             for(File f: dirOrFile.listFiles()) {
                 result.putAll(getLinks(f));
+                System.out.println(result);
 
             }
             return result;
